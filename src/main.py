f117cf24304bf060df4dd576f0ea58ff25c31a5a
Zhifeng Shi <zhifeng.shi@Zhifengs-MacBook-Pro-2.local>
2025-04-10 12:23:13 -0400

    add initial code

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from src import parlay_connect
+from src import logging
+
+
+if __name__ == '__main__':
+    logging.info("testing MM api")
+    mm_instance = parlay_connect.ParlayInteractions()
+    mm_instance.login()
+    # mm_instance.cancel_all_wagers()
+    mm_instance.get_balance()
+    mm_instance.seeding()
+    mm_instance.subscribe()
+    mm_instance.keep_alive()
+    # Jun 21, 2024, start with $908,637.13, then test batch bet/cancel to make sure all money are returned
